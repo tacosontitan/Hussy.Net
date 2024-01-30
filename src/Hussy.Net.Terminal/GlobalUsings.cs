a3fc147b70df003025d513acc808cec2501458f9
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-30 13:13:11 -0600

    Added collection aliases.

--- src/Hussy.Net.Terminal/GlobalUsings.cs
+++ src/Hussy.Net.Terminal/GlobalUsings.cs
@@ -3,24 +3,40 @@
 global using L = System.Boolean;
 
 global using S = System.Int16;
+global using SC = System.Collections.Generic.List<System.Int16>;
 global using US = System.UInt16;
+global using USC = System.Collections.Generic.List<System.UInt16>;
 
 global using I = System.Int32;
+global using IC = System.Collections.Generic.List<System.Int32>;
 global using UI = System.UInt32;
+global using UIC = System.Collections.Generic.List<System.UInt32>;
 
 global using I2 = System.Int64;
+global using I2C = System.Collections.Generic.List<System.Int64>;
 global using UI2 = System.UInt64;
+global using UI2C = System.Collections.Generic.List<System.UInt64>;
 
 global using I4 = System.Int128;
+global using I4C = System.Collections.Generic.List<System.Int128>;
 global using UI4 = System.UInt128;
+global using UI4C = System.Collections.Generic.List<System.UInt128>;
 
 global using H = System.Half;
+global using HC = System.Collections.Generic.List<System.Half>;
 global using D = System.Double;
+global using DC = System.Collections.Generic.List<System.Double>;
 global using M = System.Decimal;
+global using MC = System.Collections.Generic.List<System.Decimal>;
 
 global using B = System.Byte;
+global using BC = System.Collections.Generic.List<System.Byte>;
 global using C = System.Char;
+global using CC = System.Collections.Generic.List<System.Char>;
 global using T = System.String;
+global using TC = System.Collections.Generic.List<System.String>;
 
 global using G = System.Guid;
+global using GC = System.Collections.Generic.List<System.Guid>;
 global using BI = System.Numerics.BigInteger;
+global using BIC = System.Collections.Generic.List<System.Numerics.BigInteger>;
