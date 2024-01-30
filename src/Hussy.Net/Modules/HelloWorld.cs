4af776eee061f4c6ba801d7fbacdebf3ac7c5703
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-30 12:29:57 -0600

    Created complete hello world module.

--- src/Hussy.Net/Modules/HelloWorld.cs
+++ src/Hussy.Net/Modules/HelloWorld.cs
@@ -1,10 +1,27 @@
-﻿namespace Hussy.Net;
+﻿using Glitter;
+
+namespace Hussy.Net;
 
 /// <summary>
 /// Defines static methods for common programming tasks.
 /// </summary>
 public static partial class Hussy
 {
-    public static void Yo() =>
-        W("Hello, world!");
+    /// <summary>
+    /// Writes <c>Hello, world!</c> to the console.
+    /// </summary>
+    /// <param name="variant">The documented index for the variant to print.</param>
+    public static void Yo(int variant = 0)
+    {
+        variant.Clamp(lowerBound: 0, upperBound: _variants.Length - 1);
+        
+        string output = _variants[variant];
+        Console.Write(output);
+    }
+    
+    private static string[] _variants =
+    [
+        "Hello, world!",
+        "hello, world!"
+    ];
 }
