799e9f3f472ded639ae5408719a3446235fb6419
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-30 12:29:51 -0600

    Created complete hello world module.

--- samples/Hussy.Net.HelloWorld/Program.cs
+++ samples/Hussy.Net.HelloWorld/Program.cs
@@ -1,1 +1,2 @@
 ﻿W("Hello, world!");
+Yo();
---
+++ src/Hussy.Net/Modules/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    public static void Yo() =>
+        W("Hello, world!");
+}
