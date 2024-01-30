92bd66c59de219d7efb808cd924ba642d62bdb77
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-30 11:26:13 -0600

    Added write method.

---
+++ src/Hussy.Net.Terminal/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using static Hussy.Net.Hussy;
---
+++ src/Hussy.Net/Output/WriteLine.cs
@@ -0,0 +1,14 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static class Hussy
+{
+    /// <summary>
+    /// Writes the specified value, followed by the current line terminator, to the standard output stream.
+    /// </summary>
+    /// <param name="value">The value to write.</param>
+    public static void W<T>(T value) =>
+        Console.WriteLine(value);
+}
