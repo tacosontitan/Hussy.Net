23c9764c8677292044276d0e343accd8dadac2d8
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-30 12:16:11 -0600

    Created aliases for common types.

--- src/Hussy.Net.Terminal/GlobalUsings.cs
+++ src/Hussy.Net.Terminal/GlobalUsings.cs
@@ -1,1 +1,26 @@
 ﻿global using static Hussy.Net.Hussy;
+
+global using L = System.Boolean;
+
+global using S = System.Int16;
+global using US = System.UInt16;
+
+global using I = System.Int32;
+global using UI = System.UInt32;
+
+global using I2 = System.Int64;
+global using UI2 = System.UInt64;
+
+global using I4 = System.Int128;
+global using UI4 = System.UInt128;
+
+global using H = System.Half;
+global using D = System.Double;
+global using M = System.Decimal;
+
+global using B = System.Byte;
+global using C = System.Char;
+global using T = System.String;
+
+global using G = System.Guid;
+global using BI = System.Numerics.BigInteger;
