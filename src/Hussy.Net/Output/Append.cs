cdaa036520ab7c71a3c075d44ca11e0afd1713b8
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-30 11:29:04 -0600

    Added support for appending to the output stream.

---
+++ src/Hussy.Net/Output/Append.cs
@@ -0,0 +1,14 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Writes the text representation of the specified object to the standard output stream.
+    /// </summary>
+    /// <param name="value">The value to write.</param>
+    public static void A<T>(T value) =>
+        Console.Write(value);
+}
