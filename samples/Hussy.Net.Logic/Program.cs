b08d721490dbc2ec663304c6de52a7587d65db54
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-31 22:34:52 -0600

    Created modulus function.

--- samples/Hussy.Net.Logic/Program.cs
+++ samples/Hussy.Net.Logic/Program.cs
@@ -1,9 +1,19 @@
 ﻿// Print all even numbers from 1 to 10.
 // The function EV takes in a numeric value
 // and determines if it is even or not.
+W("Even");
 R(10).F(EV).E(W);
 
 // Print all odd numbers from 1 to 10.
 // The function OD takes in a numeric value
 // and determines if it is odd or not.
+W("\n\nOdd");
 R(10).F(OD).E(W);
+
+// Print all numbers between 1 and 100
+// that are divisible by 3 and 5.
+// The function MD extends numeric types
+// determining if the instance value
+// is divisible by all the specified operands.
+W("\n\nModulus");
+R(100).F(x => x.MD(3, 5)).E(W);
---
+++ src/Hussy.Net/Logic/Modulus.cs
@@ -0,0 +1,24 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Determines whether the specified value is divisible by all the specified operands.
+    /// </summary>
+    /// <param name="value">The value to check for divisibility.</param>
+    /// <param name="operands">The operands to check for divisibility against the value.</param>
+    /// <returns>
+    /// <c>true</c> if the value is divisible by all the operands; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool MD(this int value, params int[] operands)
+    {
+        foreach (int operand in operands)
+            if (value % operand != 0)
+                return false;
+
+        return true;
+    }
+}
