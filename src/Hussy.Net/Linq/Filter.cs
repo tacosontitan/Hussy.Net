573386c4944b159274121a014fd954fa97e08ed9
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-31 20:04:54 -0600

    Created filter function.

---
+++ src/Hussy.Net/Linq/Filter.cs
@@ -0,0 +1,17 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Filters a sequence of values based on a predicate.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the sequence.</typeparam>
+    /// <param name="source">The sequence to filter.</param>
+    /// <param name="predicate">A function to test each element for a condition.</param>
+    /// <returns>An IEnumerable{T} that contains elements from the input sequence that satisfy the condition.</returns>
+    public static IEnumerable<T> F<T>(this IEnumerable<T> source, Func<T, bool> predicate) =>
+        source.Where(predicate);
+}
