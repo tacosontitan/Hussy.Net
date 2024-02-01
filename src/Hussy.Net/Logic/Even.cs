be7b0137de4274900364607290a410967c21be98
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-31 21:53:47 -0600

    Created even function.

---
+++ src/Hussy.Net/Logic/Even.cs
@@ -0,0 +1,15 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Determines whether the given number is even.
+    /// </summary>
+    /// <param name="source">The number to check.</param>
+    /// <returns>True if the number is even; otherwise, false.</returns>
+    public static bool EV(int source) =>
+        source % 2 == 0;
+}
