681e4257c17bd619047f5a6f8111739b6ed94b5c
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-31 22:52:53 -0600

    Created output sample.

---
+++ samples/Hussy.Net.Output/Program.cs
@@ -0,0 +1,23 @@
+﻿// Hussy.Net offers many functions for writing
+// to the output stream, the simplest of which
+// is W for "write line".
+W("Hello, world!");
+
+// If you need to write a separator, then
+// the WS function can meet your needs
+// with optional character and length
+// parameters. The default character
+// is '=' and the default length is
+// 25.
+WS(character: '_', length: 30);
+
+// You can simply append to the stream
+// using the A function:
+A("Fizz");
+A("Buzz");
+
+// If you need to separate your output into
+// visible chunks, then the WH method
+// can facilitate writing a line preceded
+// by two new lines.
+WH("Sample Header");
