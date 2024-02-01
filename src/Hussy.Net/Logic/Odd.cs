d547eede1573d445ff1b32c40b44d18425411eec
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-31 21:56:56 -0600

    Created odd method.

---
+++ src/Hussy.Net/Logic/Odd.cs
@@ -0,0 +1,15 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Determines whether the given number is odd.
+    /// </summary>
+    /// <param name="source">The number to check.</param>
+    /// <returns>True if the number is odd; otherwise, false.</returns>
+    public static bool OD(int source) =>
+        source % 2 == 1;
+}
