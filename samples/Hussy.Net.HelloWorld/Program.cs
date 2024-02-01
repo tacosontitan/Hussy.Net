5b96a70ae5fc1b88a1e06fbbad01559f583eb977
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-31 22:36:49 -0600

    Updated hello world sample.

--- samples/Hussy.Net.HelloWorld/Program.cs
+++ samples/Hussy.Net.HelloWorld/Program.cs
@@ -1,11 +1,14 @@
 ﻿// Hussy has a built-in hello world module.
 // It's default output is "Hello, world!".
+W("Default");
 Yo();
 
 // If you need a common variation of the output,
 // then you can specify a variant ID to change it.
+W("\n\nDefault (variant)");
 Yo(1);
 
 // If a predefined variant doesn't exist,
 // then you can always write it from scratch:
+W("\n\nCustom");
 W("...hello...world...");
