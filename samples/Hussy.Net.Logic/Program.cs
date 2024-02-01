d5eb9971b60aa5920cd7972e3f0a67c199c07d2f
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-31 22:12:39 -0600

    Created sample project to demonstrate common logic functions.

---
+++ samples/Hussy.Net.Logic/Program.cs
@@ -0,0 +1,9 @@
+﻿// Print all even numbers from 1 to 10.
+// The function EV takes in a numeric value
+// and determines if it is even or not.
+R(10).F(EV).E(W);
+
+// Print all odd numbers from 1 to 10.
+// The function OD takes in a numeric value
+// and determines if it is odd or not.
+R(10).F(OD).E(W);
