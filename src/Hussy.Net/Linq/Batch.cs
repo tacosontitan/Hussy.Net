e9bd00c84148543d5eb1e5d8fb8431b3805eda1a
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-02-19 20:10:49 -0600

    ✨ WIP

---
+++ src/Hussy.Net/Linq/Batch.cs
@@ -0,0 +1,17 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Batches the specified <paramref name="source"/> sequence into chunks of the specified <paramref name="size"/>.
+    /// </summary>
+    /// <param name="source">Specifies the source sequence to batch.</param>
+    /// <param name="size">Specifies the size of the individual batches.</param>
+    /// <typeparam name="T">Specifies the type of the data in the <paramref name="source"/> sequence.</typeparam>
+    /// <returns>A collection of batches created from the specified <paramref name="source"/> sequence.</returns>
+    public static IEnumerable<IEnumerable<T>> B<T>(this IEnumerable<T> source, int size = 2) =>
+        source.Chunk(size);
+}
---
+++ src/Hussy.Net/Text/ArrayFormat.cs
@@ -0,0 +1,36 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Creates a classic array string (e.g. <c>[1, 2, 3]</c>) for the specified sequence.
+    /// </summary>
+    /// <param name="sequence">The sequence to create the array string for.</param>
+    /// <typeparam name="T">Specifies the type of data in the sequence.</typeparam>
+    /// <returns>The input sequence wrapped in square brackets and separated by commas and spaces.</returns>
+    /// <example>
+    /// Behind the scenes, this method simply interpolates a call to <c>string.Join</c> with <c>", "</c> as the
+    /// separator. This results in an output string of <c>[1, 2, 3, ...]</c>, to use it, simply specify a collection
+    /// parameter:
+    ///
+    /// <code>
+    /// // Generate a range to work with:
+    /// var sequence = Gr(5);
+    ///
+    /// // Create the formatted string:
+    /// var output = Far(sequence);
+    ///
+    /// // Append the output to the output stream:
+    /// A(output);
+    /// </code>
+    ///
+    /// The expected output of the above snippet is:
+    ///
+    /// > [1, 2, 3, 4, 5]
+    /// </example>
+    public static string Far<T>(IEnumerable<T> sequence) =>
+        $"[{string.Join(", ", sequence)}]";
+}
