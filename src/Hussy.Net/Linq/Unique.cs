6c6951954343c16e4cf8feeb95f33d2b06b3054c
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-02-20 12:35:44 -0600

    Created Unique extension for collections that calls Distinct.

---
+++ src/Hussy.Net/Linq/Unique.cs
@@ -0,0 +1,16 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Gets the unique elements in the collection.
+    /// </summary>
+    /// <param name="source">The collection to get unique elements from.</param>
+    /// <typeparam name="T">Specifies the type of data in the collection.</typeparam>
+    /// <returns>The unique elements in the collection.</returns>
+    public static IEnumerable<T> U<T>(this IEnumerable<T> source) =>
+        source.Distinct();
+}
