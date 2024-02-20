ffacedb20ccf9765a20d2a06673069cac9748c98
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-02-20 11:51:53 -0600

    Added dry run for the C270154 golfing challenge on Stack Exchange.

---
+++ samples/Hussy.Net.Playground/C270154/Tests/TestThree.cs
@@ -0,0 +1,23 @@
+﻿namespace Hussy.Net.Playground;
+
+/// <summary>
+/// Represents a solution and its tests for the "Strings without twin letters" challenge on Code Golf SE.
+/// </summary>
+/// <see href="https://codegolf.stackexchange.com/questions/270154/strings-without-twin-letters"/>
+public sealed partial class C270154
+{
+    [Fact]
+    public void TestCaseThree_Passes()
+    {
+        var length = 3;
+        var characters = "ABCD";
+        var expectedResult = new[]
+        {
+            "ABA", "ABC", "ABD", "ACA", "ACB", "ACD", "ADA", "ADB", "ADC", "BAB", "BAC", "BAD", "BCA", "BCB", "BCD",
+            "BDA", "BDB", "BDC", "CAB", "CAC", "CAD", "CBA", "CBC", "CBD", "CDA", "CDB", "CDC", "DAB", "DAC", "DAD",
+            "DBA", "DBC", "DBD", "DCA", "DCB", "DCD"
+        };
+
+        RunTest(expectedResult, characters, length);
+    }
+}
