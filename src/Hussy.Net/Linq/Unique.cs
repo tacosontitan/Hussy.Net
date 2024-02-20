3367d9dec4e8b00ebdf2874752bc7771bde11820
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-02-20 12:36:03 -0600

    Created unique or empty extension for collections.

--- src/Hussy.Net/Linq/Unique.cs
+++ src/Hussy.Net/Linq/Unique.cs
@@ -13,4 +13,15 @@ public static partial class Hussy
     /// <returns>The unique elements in the collection.</returns>
     public static IEnumerable<T> U<T>(this IEnumerable<T> source) =>
         source.Distinct();
+    
+    /// <summary>
+    /// Gets the unique or empty version of the source collection.
+    /// </summary>
+    /// <param name="source">The input sequence to use.</param>
+    /// <typeparam name="T">Specifies the type of data in the collection.</typeparam>
+    /// <returns>The source collection if all elements are unique, otherwise an empty collection.</returns>
+    public static IEnumerable<T> Uoe<T>(this IEnumerable<T> source) =>
+        source.Distinct().Count() < source.Count()
+            ? Enumerable.Empty<T>() 
+            : source;
 }
