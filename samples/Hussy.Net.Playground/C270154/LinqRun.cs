2007a317f2048315642ac664ec867d42d3f46f8e
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-02-20 11:51:53 -0600

    Added dry run for the C270154 golfing challenge on Stack Exchange.

---
+++ samples/Hussy.Net.Playground/C270154/LinqRun.cs
@@ -0,0 +1,17 @@
+﻿namespace Hussy.Net.Playground;
+
+/// <summary>
+/// Represents a solution and its tests for the "Strings without twin letters" challenge on Code Golf SE.
+/// </summary>
+/// <see href="https://codegolf.stackexchange.com/questions/270154/strings-without-twin-letters"/>
+public sealed partial class C270154
+{
+    /// <summary>
+    /// Tackles the problem using just LINQ methods.
+    /// </summary>
+    private static IEnumerable<string> LinqRun(string characters, int length) => Enumerable.Range(1, length - 1)
+        .Aggregate(characters.Select(c => c.ToString()), (current, _) =>
+            current.SelectMany(prefix =>
+                    characters.Where(c => c != prefix.LastOrDefault()),
+                (prefix, nextChar) => prefix + nextChar));
+}
---
+++ samples/Hussy.Net.Playground/C270154/Tests/Setup.cs
@@ -0,0 +1,36 @@
+﻿namespace Hussy.Net.Playground;
+
+/// <summary>
+/// Represents a solution and its tests for the "Strings without twin letters" challenge on Code Golf SE.
+/// </summary>
+/// <see href="https://codegolf.stackexchange.com/questions/270154/strings-without-twin-letters"/>
+public sealed partial class C270154
+{
+    /// <summary>
+    /// Gets the functions the tests should be applied to.
+    /// </summary>
+    private static TestFunction[] TestFunctions =>
+    [
+        DryRun
+    ];
+    
+    /// <summary>
+    /// Tests all runs to ensure each has the same expected result.
+    /// </summary>
+    private static void RunTest(
+        IEnumerable<string> expectedResults,
+        string characters,
+        int length)
+    {
+        var expectedSequence = expectedResults.ToList();
+        foreach (var testFunction in TestFunctions)
+        {
+            var results = testFunction(characters, length);
+            var resultingSequence = results.ToList();
+
+            var excessResults = resultingSequence.Except(expectedSequence);
+            Assert.True(expectedSequence.All(resultingSequence.Contains));
+            Assert.Empty(excessResults);
+        }
+    }
+}
