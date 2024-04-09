b43ea07c44b6e711e7e4b0ac8cf6e0d95cf4caa2
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-08 20:36:01 -0500

    Defined leetcode test cases for the two sum module.

---
+++ samples/Hussy.Net.Playground/Leetcode/Easy/TwoSum/Tests/TestThree.cs
@@ -0,0 +1,14 @@
+﻿namespace Hussy.Net.Playground.Leetcode.Easy;
+
+/// <summary>
+/// Represents a solution and its tests for the leetcode two-sum problem.
+/// </summary>
+/// <see href="https://leetcode.com/problems/two-sum/description/"/>
+public sealed partial class TwoSum
+{
+    [Fact]
+    public void TestCaseThree_Passes() => RunTest(
+        expectedResults: [0, 1],
+        numbers: [3, 3],
+        target: 6);
+}
---
+++ samples/Hussy.Net.Playground/Leetcode/Easy/TwoSum/Tests/TestTwo.cs
@@ -0,0 +1,14 @@
+﻿namespace Hussy.Net.Playground.Leetcode.Easy;
+
+/// <summary>
+/// Represents a solution and its tests for the leetcode two-sum problem.
+/// </summary>
+/// <see href="https://leetcode.com/problems/two-sum/description/"/>
+public sealed partial class TwoSum
+{
+    [Fact]
+    public void TestCaseTwo_Passes() => RunTest(
+        expectedResults: [1, 2],
+        numbers: [3, 2, 4],
+        target: 6);
+}
