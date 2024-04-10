927e3f50ccd8f1b582808bcb6e27d53fbb82a94b
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 17:41:38 -0500

    Created cartesian for-each extensions.

---
+++ src/Hussy.Net/Iterators/ForEach/Cartesian/ThreeTermCartesianForEach.cs
@@ -0,0 +1,27 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines static methods for common programming tasks.
+/// </summary>
+public static partial class Hussy
+{
+    /// <summary>
+    /// Applies the specified action to each element in the source collection.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the source collection.</typeparam>
+    /// <param name="source">The source collection.</param>
+    /// <param name="action">The action to be applied to each element.</param>
+    /// <returns>The source collection.</returns>
+    public static IEnumerable<T> E<T>(
+        this IEnumerable<T> source,
+        Action<int, int, int, T> action)
+    {
+        var elements = source.ToList();
+        for (var x = 0; x < elements.Count; x++)
+        for (var y = 0; y < elements.Count; y++)
+        for (var z = 0; z < elements.Count; z++)
+            action(x, y, z, elements[x]);
+
+        return elements;
+    }
+}
