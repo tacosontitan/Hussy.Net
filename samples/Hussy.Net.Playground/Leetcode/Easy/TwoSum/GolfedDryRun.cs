800cc4991c6b6c67440eed9d9c7989cb2f7e4a4f
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 15:56:04 -0500

    Created golfed dry run.

---
+++ samples/Hussy.Net.Playground/Leetcode/Easy/TwoSum/GolfedDryRun.cs
@@ -0,0 +1,32 @@
+﻿namespace Hussy.Net.Playground.Leetcode.Easy;
+
+/// <summary>
+/// Represents a solution and its tests for the leetcode two-sum problem.
+/// </summary>
+/// <see href="https://leetcode.com/problems/two-sum/description/"/>
+public sealed partial class TwoSum
+{
+    /// <summary>
+    ///     A simple golf run to prepare for the Hussy run.
+    /// </summary>
+    /// <param name="n">The array of numbers to look through.</param>
+    /// <param name="t">
+    ///     The target number which the two numbers in <paramref name="n"/> must add up to.
+    /// </param>
+    /// <returns>
+    ///     The two indices of the numbers which add up to the <paramref name="t"/>.
+    /// </returns>
+    private static IEnumerable<int> GolfedDryRun(
+        int[] n,
+        int t)
+    {
+        // Fully condensed, this method body becomes
+        // 93 bytes.
+        for (int x = 0, l = n.Length; x < l; x++)
+        for (int y = 1; x != y & y < l; y++)
+            if (n[x] + n[y] == t)
+                return [x, y];
+
+        return [];
+    }
+}
