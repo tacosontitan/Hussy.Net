8be6e18ee4dab5ea03a232a3a568b7ec91d2d425
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 19:48:34 -0500

    Created an indexed mapping function.

---
+++ src/Hussy.Net/Linq/Map/IndexedMap.cs
@@ -0,0 +1,29 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    /// Applies a mapping function to each element in the source enumerable and returns an enumerable
+    /// containing the result of the mapping.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source enumerable.</typeparam>
+    /// <typeparam name="TResult">The type of the elements in the resulting enumerable.</typeparam>
+    /// <param name="source">The source enumerable.</param>
+    /// <param name="mappingFunction">The mapping function to apply to each element.</param>
+    /// <returns>An enumerable containing the results of the mapping.</returns>
+    public static IEnumerable<TResult> M<TSource, TResult>(
+        this IEnumerable<TSource> source,
+        Func<int, TSource, TResult> mappingFunction)
+    {
+        var index = 0;
+        var sequence = source.ToList();
+        var results = new List<TResult>(capacity: sequence.Count);
+        foreach (var element in sequence)
+        {
+            var result = mappingFunction(index++, element);
+            results.Add(result);
+        }
+
+        return results;
+    }
+}
