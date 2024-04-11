7bbb9cebe31568cd7b1fcde659fcb7eb9924f8d4
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 19:45:15 -0500

    Created a basic indexed primitive.

---
+++ src/Hussy.Net/Primitives/Indexed/Definition.cs
@@ -0,0 +1,20 @@
+﻿namespace Hussy.Net;
+
+/// <summary>
+/// Defines an indexed value for simplifying working with collections.
+/// </summary>
+/// <typeparam name="T">Specifies the type of the value being indexed.</typeparam>
+public partial class Indexed<T>(
+    int index,
+    T value)
+{
+    /// <summary>
+    /// Gets or sets the index of this element.
+    /// </summary>
+    public int Index { get; set; } = index;
+
+    /// <summary>
+    /// Gets or sets the value of this indexed element.
+    /// </summary>
+    public T Value { get; set; } = value;
+}
