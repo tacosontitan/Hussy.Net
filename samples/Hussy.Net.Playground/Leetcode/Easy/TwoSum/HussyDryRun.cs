e1d4ee8616d21697bc63b1aaf15946048812c80f
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-11 07:41:01 -0500

    Formatted hussy two-sum run for legibility.

--- samples/Hussy.Net.Playground/Leetcode/Easy/TwoSum/HussyDryRun.cs
+++ samples/Hussy.Net.Playground/Leetcode/Easy/TwoSum/HussyDryRun.cs
@@ -9,15 +9,18 @@ public sealed partial class TwoSum
     /// <summary>
     /// A very mundane and direct approach to solving the problem to understand where Hussy may be lacking.
     /// </summary>
-    /// <param name="numbers">The array of numbers to look through.</param>
-    /// <param name="target">
-    ///     The target number which the two numbers in <paramref name="numbers"/> must add up to.
+    /// <param name="n">The array of numbers to look through.</param>
+    /// <param name="t">
+    ///     The target number which the two numbers in <paramref name="n"/> must add up to.
     /// </param>
     /// <returns>
-    ///     The two indices of the numbers which add up to the <paramref name="target"/>.
+    ///     The two indices of the numbers which add up to the <paramref name="t"/>.
     /// </returns>
     private static IEnumerable<int>? HussyDryRun(
         int[] n,
         int t) =>
-        n.Fe((x, y) => x.I != y.I && x.V + y.V == t,(x, y) => new[] { x.I, y.I });
+        n.Fe(
+            (x, y) => x.I != y.I && x.V + y.V == t,
+            (x, y) => new[] { x.I, y.I }
+        );
 }
