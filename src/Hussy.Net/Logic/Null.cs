4a9cebc31878a96a153efe3869e15186eeba448e
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 22:00:08 -0500

    Created null function.

---
+++ src/Hussy.Net/Logic/Null.cs
@@ -0,0 +1,18 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    /// Determines whether the specified value is <see langword="null"/> or not.
+    /// </summary>
+    /// <param name="value">The value to check for <see langword="null"/>.</param>
+    /// <typeparam name="T">Specifies the type of the value being checked.</typeparam>
+    /// <returns>
+    ///     <see langword="true"/> if <paramref name="value"/> is
+    ///     <see langword="null"/>; otherwise,
+    ///     <see langword="false"/>.
+    /// </returns>
+    public static bool Nul<T>(T? value)
+        where T : class =>
+        value is null;
+}
