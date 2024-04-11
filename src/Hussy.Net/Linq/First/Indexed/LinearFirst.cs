39309e4e351a0cdaa264681849ee82aae9a65a5b
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 22:01:29 -0500

    ✨WIP: Indexed first methods.

---
+++ src/Hussy.Net/Linq/First/Indexed/LinearFirst.cs
@@ -0,0 +1,30 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    /// Gets the first element of the source collection.
+    /// </summary>
+    /// <param name="source">The source collection to get the first element from.</param>
+    /// <param name="predicate">A predicate which determines if the element is a match.</param>
+    /// <typeparam name="T">Specifies the type of data within the collection.</typeparam>
+    /// <returns>
+    ///     The first element in the source collection if elements are present;
+    ///     otherwise the default value for <typeparamref name="T"/>.
+    /// </returns>
+    public static T? Fe<T>(
+        this IEnumerable<T> source,
+        Func<Indexed<T>, bool> predicate)
+    {
+        var index = 0;
+        var sequence = source.ToList();
+        foreach (var element in sequence)
+        {
+            var indexedElement = new Indexed<T>(index, element);
+            if (predicate(indexedElement))
+                return element;
+        }
+
+        return default;
+    }
+}
---
+++ src/Hussy.Net/Linq/First/Indexed/Mapped/LinearMapped.cs
@@ -0,0 +1,36 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    /// Gets and maps the first element of the source collection.
+    /// </summary>
+    /// <param name="source">The source collection to get the first element from.</param>
+    /// <param name="predicate">A predicate which determines if the element is a match.</param>
+    /// <param name="mappingFunction">A function for mapping the matched predicate data to the intended result.</param>
+    /// <typeparam name="TSource">Specifies the type of data within the collection.</typeparam>
+    /// <typeparam name="TResult">Specifies the type of the result of the mapping function.</typeparam>
+    /// <returns>
+    ///     The first element in the collection matching the predicate,
+    ///     mapped with the specified mapping function.
+    /// </returns>
+    public static TResult? Fe<TSource, TResult>(
+        this IEnumerable<TSource> source,
+        Func<Indexed<TSource>, bool> predicate,
+        Func<Indexed<TSource>, TResult> mappingFunction)
+    {
+        var index = 0;
+        var sequence = source.ToList();
+        foreach (var element in sequence)
+        {
+            var indexedElement = new Indexed<TSource>(index, element);
+            if (predicate(indexedElement))
+            {
+                var result = mappingFunction(indexedElement);
+                return result;
+            }
+        }
+
+        return default;
+    }
+}
