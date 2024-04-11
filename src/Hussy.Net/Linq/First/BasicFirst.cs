fe5fd901e1e39cb36923ceaf43bf48d806ae6de7
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 20:44:54 -0500

    Renamed first.

---
+++ src/Hussy.Net/Linq/First/BasicFirst.cs
@@ -0,0 +1,16 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    /// Gets the first element of the source collection.
+    /// </summary>
+    /// <param name="source">The source collection to get the first element from.</param>
+    /// <typeparam name="T">Specifies the type of data within the collection.</typeparam>
+    /// <returns>
+    ///     The first element in the source collection if elements are present;
+    ///     otherwise the default value for <typeparamref name="T"/>.
+    /// </returns>
+    public static T? Fe<T>(this IEnumerable<T> source) =>
+        source.FirstOrDefault();
+}
