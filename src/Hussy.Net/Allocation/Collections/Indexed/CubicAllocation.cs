3768a805f4c467eb2fe5307edbb9db020db6aab9
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 22:02:00 -0500

    ✨WIP: Collection allocation methods.

---
+++ src/Hussy.Net/Allocation/Collections/Indexed/CubicAllocation.cs
@@ -0,0 +1,19 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    /// Determines whether the specified value is <see langword="null"/> or not.
+    /// </summary>
+    /// <param name="value">The value to check for <see langword="null"/>.</param>
+    /// <typeparam name="T">Specifies the type of the value being checked.</typeparam>
+    /// <returns>
+    ///     <see langword="true"/> if <paramref name="value"/> is not
+    ///     <see langword="null"/>; otherwise,
+    ///     <see langword="false"/>.
+    /// </returns>
+    public static C<T> Ac<T>(
+        Indexed<T> x,
+        Indexed<T> y) =>
+        new() { x, y };
+}
