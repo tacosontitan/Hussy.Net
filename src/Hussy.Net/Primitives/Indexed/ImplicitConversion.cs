fc4cc00dab9d15bceef7363abe3f16a2fead05df
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 19:45:15 -0500

    Created a basic indexed primitive.

---
+++ src/Hussy.Net/Primitives/Indexed/ImplicitConversion.cs
@@ -0,0 +1,20 @@
+﻿namespace Hussy.Net;
+
+public partial class Indexed<T>
+{
+    /// <summary>
+    /// Implicitly converts an indexed value to its value component.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    /// <returns>The value of the indexed value.</returns>
+    public static implicit operator T(Indexed<T> value) =>
+        value.Value;
+
+    /// <summary>
+    /// Implicitly converts an indexed value to its index component.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    /// <returns>The index of the indexed value.</returns>
+    public static implicit operator int(Indexed<T> value) =>
+        value.Index;
+}
