289678bc0e28c883cecf015fe0846580f4b76c9d
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-10 19:43:46 -0500

    Created basic index of method.

---
+++ src/Hussy.Net/Linq/Index/BasicIndexOf.cs
@@ -0,0 +1,29 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    /// Gets the index of the specified element within the source collection.
+    /// </summary>
+    /// <param name="source">The source collection to look through.</param>
+    /// <param name="element">The element to look for.</param>
+    /// <typeparam name="T">Specifies the type of data within the source collection.</typeparam>
+    /// <returns>
+    ///     The index of the element within the collection if found;
+    ///     otherwise <c>-1</c>.
+    /// </returns>
+    public static int I<T>(
+        this IEnumerable<T> source,
+        T element)
+    {
+        var sequence = source.ToList();
+        for (var index = 0; index < sequence.Count; index++)
+        {
+            var elementAtIndex = sequence[index];
+            if (element.Equals(elementAtIndex))
+                return index;
+        }
+
+        return -1;
+    }
+}
