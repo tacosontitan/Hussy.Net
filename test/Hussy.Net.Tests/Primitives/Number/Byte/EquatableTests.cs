7ebec0a64f1e80cb25db8f2a52b07eaaddb858d0
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-13 14:54:40 -0500

    Tested byte equality.

---
+++ test/Hussy.Net.Tests/Primitives/Number/Byte/EquatableTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Hussy.Net.Tests.Primitives;
+
+public sealed class NumberTests
+{
+    [Fact]
+    public void EqualsByte_Equal_ReturnsTrue()
+    {
+        const byte testValue = 5;
+        Number sample = testValue;
+        Assert.True(sample.Equals(testValue));
+    }
+
+    [Fact]
+    public void EqualsByte_NotEqual_ReturnsFalse()
+    {
+        const byte testValue = 3;
+        Number sample = testValue;
+        Assert.Equal(testValue, sample);
+
+        const byte comparisonValue = 5;
+        Assert.NotEqual(comparisonValue, sample);
+    }
+}
