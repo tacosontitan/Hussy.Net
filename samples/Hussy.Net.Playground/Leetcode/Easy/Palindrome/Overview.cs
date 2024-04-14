5faa9d431681fc90f551a819ea524a32534a1be8
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-13 21:29:33 -0500

    Created the overview for the palindrome problem.

---
+++ samples/Hussy.Net.Playground/Leetcode/Easy/Palindrome/Overview.cs
@@ -0,0 +1,20 @@
+﻿namespace Hussy.Net.Playground.Leetcode.Easy.Palindrome;
+
+/// <summary>
+/// Represents a solution and its tests for the leetcode palindrome problem.
+/// </summary>
+/// <see href="https://leetcode.com/problems/palindrome-number/description/"/>
+public sealed partial class Overview
+{
+    /// <summary>
+    ///     Given an integer <paramref name="input"/>, return <see langword="true"/>
+    ///     if the number is a palindrome; otherwise, <see langword="false"/>.
+    /// </summary>
+    /// <param name="input">The <see cref="int"/> instance to evaluate.</param>
+    /// <returns>
+    ///     <see langword="true"/> if the number is a palindrome;
+    ///     otherwise, <see langword="false"/>.
+    /// </returns>
+    /// <see href="https://en.wikipedia.org/wiki/Palindrome"/>
+    private delegate bool TestFunction(int input);
+}
