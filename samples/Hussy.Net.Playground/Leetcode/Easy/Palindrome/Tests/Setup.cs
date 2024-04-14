dd257a777aa05d408081e4115a4edadfd8558f64
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-13 21:51:00 -0500

    Added initial golfed dry run to the palindrome module.

--- samples/Hussy.Net.Playground/Leetcode/Easy/Palindrome/Tests/Setup.cs
+++ samples/Hussy.Net.Playground/Leetcode/Easy/Palindrome/Tests/Setup.cs
@@ -4,7 +4,8 @@ public sealed partial class Palindrome
 {
     private static TestFunction[] TestFunctions =>
     [
-        DryRun
+        DryRun,
+        GolfedDryRun
     ];
 
     private static void RunTest(
