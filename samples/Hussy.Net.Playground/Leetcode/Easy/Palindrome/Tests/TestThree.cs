8c146bc6c9e0122caa9836657d4033bf0529bf6c
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-13 21:40:36 -0500

    Added third test case for the palindrome module.

---
+++ samples/Hussy.Net.Playground/Leetcode/Easy/Palindrome/Tests/TestThree.cs
@@ -0,0 +1,11 @@
+﻿namespace Hussy.Net.Playground.Leetcode.Easy;
+
+public sealed partial class Palindrome
+{
+    [Fact]
+    public void TestCaseThree_Passes()
+    {
+        const int input = 10;
+        RunTest(expectedResult: true, input);
+    }
+}
