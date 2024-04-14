cf4e24698acdf1fe3effeb8c9309157cdf601ff0
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-14 11:58:33 -0500

    Defined fizzfizzfizzbuzz module in the playground.

---
+++ samples/Hussy.Net.Playground/Golfing/C272452/Overview.cs
@@ -0,0 +1,38 @@
+﻿namespace Hussy.Net.Playground.Golfing;
+
+/// <summary>
+/// Represents a solution and its tests for the "fizz fizz fizz buzz" challenge on Code Golf SE.
+/// </summary>
+/// <see href="https://codegolf.stackexchange.com/questions/272452/fizzfizzfizzbuzz"/>
+public sealed partial class C272452
+{
+    /// <summary>
+    ///     <para>
+    ///         Given a positive integer <paramref name="target"/>, output
+    ///         the "<b>FizzFizzFizzBuzz</b>" value for that number.
+    ///     </para>
+    ///     <para>
+    ///         <b>Requirements:</b>
+    ///         <list type="bullet">
+    ///             <item>
+    ///                 If the number is divisible by both <c>3</c> <i>and</i> <c>5</c>,
+    ///                 instead of the number, output <c>Fizz</c> the number of times
+    ///                 it can be divided by <c>3</c>, then <c>Buzz</c> the number of
+    ///                 times it can be divided by <c>5</c>.
+    ///             </item>
+    ///             <item>
+    ///                 If the number is divisible by <c>3</c>, instead of the number,
+    ///                 output <c>Fizz</c> the number of times it can be divided by <c>3</c>.
+    ///             </item>
+    ///             <item>
+    ///                 If the number is divisible by <c>5</c>, instead of the number,
+    ///                 output <c>Buzz</c> the number of times it can be divided by <c>5</c>.
+    ///             </item>
+    ///             <item>
+    ///                 Otherwise, output the number itself.
+    ///             </item>
+    ///         </list>
+    ///     </para>
+    /// </summary>
+    private delegate IEnumerable<string> TestFunction(int target);
+}
---
+++ samples/Hussy.Net.Playground/Golfing/C272452/Tests/Setup.cs
@@ -0,0 +1,25 @@
+﻿namespace Hussy.Net.Playground.Golfing;
+
+public sealed partial class C272452
+{
+    private static TestFunction[] TestFunctions =>
+    [
+        
+    ];
+
+    private static void RunTests(
+        IEnumerable<string> expectedResults,
+        int target)
+    {
+        var expectedSequence = expectedResults.ToList();
+        foreach (var testFunction in TestFunctions)
+        {
+            var results = testFunction(target);
+            var resultingSequence = results.ToList();
+
+            var excessResults = resultingSequence.Except(expectedSequence);
+            Assert.True(expectedSequence.All(resultingSequence.Contains));
+            Assert.Empty(excessResults);
+        }
+    }
+}
