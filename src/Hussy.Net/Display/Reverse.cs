cfc835bb4b4a1a6b58676b618656a7b3ab699ecb
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-14 09:41:03 -0500

    Created a method to reverse a given input.

---
+++ src/Hussy.Net/Display/Reverse.cs
@@ -0,0 +1,24 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    ///     Converts the specified <paramref name="input"/>
+    ///     to a <see cref="string"/> and reverses it.
+    /// </summary>
+    /// <param name="input">
+    ///     The <paramref name="input"/> to reverse.
+    /// </param>
+    /// <typeparam name="T">
+    ///     Specifies the type of the <paramref name="input"/> to reverse.
+    /// </typeparam>
+    /// <returns>
+    ///     The reversal of the specified <paramref name="input"/>.
+    /// </returns>
+    public static string Rev<T>(T input)
+    {
+        var stringValue = input.ToString();
+        var reversedSequence = stringValue.Reverse().ToArray();
+        return new string(reversedSequence);
+    }
+}
