55957cb1573fafc73b5122fdc60e918aa75d9c94
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-13 21:51:00 -0500

    Added initial golfed dry run to the palindrome module.

---
+++ samples/Hussy.Net.Playground/Leetcode/Easy/Palindrome/GolfedDryRun.cs
@@ -0,0 +1,19 @@
+﻿namespace Hussy.Net.Playground.Leetcode.Easy;
+
+public sealed partial class Palindrome
+{
+    private static bool GolfedDryRun(int i)
+    {
+        if (i < 0)
+            return false;
+
+        if (i / 10 == 0)
+            return true;
+
+        int t = i, r = t % 10;
+        while (t / 10 > 0)
+            r = r * 10 + (t /= 10) % 10;
+
+        return r == i;
+    }
+}
