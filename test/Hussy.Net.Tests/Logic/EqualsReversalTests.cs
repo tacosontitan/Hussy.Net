654782a0d009afd0b7060b9335eac454015605e6
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-14 10:01:41 -0500

    Defined tests for equals reversal.

---
+++ test/Hussy.Net.Tests/Logic/EqualsReversalTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Hussy.Net.Tests.Logic;
+
+public class EqualsReversalTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(0)]
+    [InlineData(121)]
+    [InlineData("abba")]
+    public void EqualsReversal_InputMatchesReversal_ReturnsTrue(object testValue)
+    {
+        var reversalEqualsTestValue = testValue.EqRev();
+        Assert.True(reversalEqualsTestValue);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData("123")]
+    public void EqualsReversal_InputDoesNotMatchReversal_ReturnsFalse(object testValue)
+    {
+        var reversalEqualsTestValue = testValue.EqRev();
+        Assert.False(reversalEqualsTestValue);
+    }
+}
