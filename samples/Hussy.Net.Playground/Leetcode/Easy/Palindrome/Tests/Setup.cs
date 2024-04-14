74eb40f98c40aacc5e0f2d02c45e052a42580acd
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-13 21:36:01 -0500

    Added setup for tests.

---
+++ samples/Hussy.Net.Playground/Leetcode/Easy/Palindrome/Tests/Setup.cs
@@ -0,0 +1,20 @@
+﻿namespace Hussy.Net.Playground.Leetcode.Easy;
+
+public sealed partial class Overview
+{
+    private static TestFunction[] TestFunctions =>
+    [
+        
+    ];
+
+    private static void RunTest(
+        bool expectedResult,
+        int input)
+    {
+        foreach (var isPalindrome in TestFunctions.Select(IsPalindrome))
+            Assert.Equal(expectedResult, isPalindrome);
+
+        bool IsPalindrome(TestFunction testFunction) =>
+            testFunction(input);
+    }
+}
