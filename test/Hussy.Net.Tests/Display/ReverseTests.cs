29850ecbf6e02afd469944060938c58f28280d77
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-14 09:44:33 -0500

    Defined a test for the reverse method.

---
+++ test/Hussy.Net.Tests/Display/ReverseTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Hussy.Net.Tests.Display;
+
+public class ReverseTests
+{
+    [Fact]
+    public void Reverse_ValidInput_ReversesInput()
+    {
+        const string testValue = "abc";
+        var reversal = Rev(testValue);
+
+        const string expectation = "cba";
+        Assert.Equal(expectation, reversal);
+    }
+}
