8b99e7811b6f538d83d6024e26541d0372099968
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-14 09:53:10 -0500

    Created tests for the palindrome module.

---
+++ test/Hussy.Net.Tests/Modules/PalindromeTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Hussy.Net.Tests.Modules;
+
+public class PalindromeTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(55)]
+    [InlineData(121)]
+    [InlineData(1221)]
+    public void Palindrome_IsPalindrome_ReturnsTrue(int testValue)
+    {
+        var isPalindrome = Pal(testValue);
+        Assert.True(isPalindrome);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(12)]
+    [InlineData(123)]
+    [InlineData(1223)]
+    public void Palindrome_IsNotPalindrome_ReturnsFalse(int testValue)
+    {
+        var isPalindrome = Pal(testValue);
+        Assert.False(isPalindrome);
+    }
+}
