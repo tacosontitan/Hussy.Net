476954915638c8eeb934a8e0cc0d767a5716c0f0
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-13 21:39:41 -0500

    Added second test case for the palindrome module.

---
+++ samples/Hussy.Net.Playground/Leetcode/Easy/Palindrome/Tests/TestTwo.cs
@@ -0,0 +1,11 @@
+﻿namespace Hussy.Net.Playground.Leetcode.Easy;
+
+public sealed partial class Palindrome
+{
+    [Fact]
+    public void TestCaseTwo_Passes()
+    {
+        const int input = -121;
+        RunTest(expectedResult: false, input);
+    }
+}
