63cbd78f30952397e5ceaf4782863ed906cbf12f
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-14 19:57:28 -0500

    Created a basic for imitation.

---
+++ src/Hussy.Net/Iterators/For/BasicFor.cs
@@ -0,0 +1,26 @@
+﻿namespace Hussy.Net;
+
+public static partial class Hussy
+{
+    /// <summary>
+    /// Imitates a <see langword="for"/> loop.
+    /// </summary>
+    /// <param name="startingValue">The value to start iterating with.</param>
+    /// <param name="iterationCondition">The condition for each iteration to execute.</param>
+    /// <param name="iterator">The mutation that should be applied to the iterator value.</param>
+    /// <param name="action">The action to invoke with every iteration.</param>
+    /// <typeparam name="T">Specifies the type of data the iterator works with.</typeparam>
+    public static void F<T>(
+        T startingValue,
+        Func<T, bool> iterationCondition,
+        Func<T, T> iterator,
+        Action<T> action)
+    {
+        var currentValue = startingValue;
+        while (iterationCondition(currentValue))
+        {
+            action(currentValue);
+            currentValue = iterator(currentValue);
+        }
+    }
+}
