e0db2374f7c6a9630305667793e3234a9ff86c17
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-14 21:05:07 -0500

    Added overload for append that accepts a fallback for null.

--- src/Hussy.Net/Output/Append.cs
+++ src/Hussy.Net/Output/Append.cs
@@ -27,4 +27,28 @@ public static partial class Hussy
     /// <param name="value">The value to write.</param>
     public static void A<T>(T value) =>
         Console.Write(value);
+
+    /// <summary>
+    ///     Appends the specified <paramref name="primaryValue"/> if it is not
+    ///     <see langword="null"/>; otherwise, the specified <paramref name="fallbackValue"/>
+    ///     is written.
+    /// </summary>
+    /// <param name="primaryValue">The primary value to write.</param>
+    /// <param name="fallbackValue">
+    ///     The value to write if the <paramref name="primaryValue"/> is <see langword="null"/>.
+    /// </param>
+    /// <typeparam name="TPrimary">Specifies the type of the primary value to write.</typeparam>
+    /// <typeparam name="TFallback">Specifies the type of the fallback value to write.</typeparam>
+    public static void A<TPrimary, TFallback>(
+        TPrimary primaryValue,
+        TFallback fallbackValue)
+    {
+        if (primaryValue is not null)
+        {
+            Console.Write(primaryValue);
+            return;
+        }
+        
+        Console.Write(fallbackValue);
+    }
 }
