4469246b1dd0c1cbd593b6604be21b2587c1e34a
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-17 19:12:48 -0500

    Added documentation for the to string extension.

--- src/Hussy.Net/Display/ToString.cs
+++ src/Hussy.Net/Display/ToString.cs
@@ -2,6 +2,15 @@
 
 public static partial class Hussy
 {
+    /// <summary>
+    /// Converts the specified input to its <see cref="string"/> representation.
+    /// </summary>
+    /// <param name="input">The input to convert.</param>
+    /// <typeparam name="T">Specifies the type of the <paramref name="input"/> to convert.</typeparam>
+    /// <returns>
+    ///     The <see cref="string"/> representation of the specified
+    ///     <paramref name="input"/> instance.
+    /// </returns>
     public static string Ts<T>(this T input) =>
         input.ToString();
 }
