5b41a7d3c1133be1ab463c7e2425d9e5596972be
tacosontitan <contact@tacosontitan.com>
2024-07-20 21:39:12 -0500

    Completed basic support for tostring.

--- src/Hussy.Net/Display/ToString.cs
+++ src/Hussy.Net/Display/ToString.cs
@@ -9,8 +9,13 @@ public static partial class Hussy
     /// <typeparam name="T">Specifies the type of the <paramref name="input"/> to convert.</typeparam>
     /// <returns>
     ///     The <see cref="string"/> representation of the specified
-    ///     <paramref name="input"/> instance.
+    ///     <paramref name="input"/> instance. A value of
+    ///     <see langword="null"/> will return a <see cref="string"/>
+    ///     containing the word <see langword="null"/>.
     /// </returns>
-    public static string Ts<T>(this T input) =>
-        input.ToString();
+    public static string Ts<T>(this T input)
+    {
+        var result = input?.ToString();
+        return result ?? "null";
+    }
 }
---
+++ test/Hussy.Net.Tests/Display/ToStringTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Hussy.Net.Tests.Display;
+
+public class ToStringTests
+{
+    [Fact]
+    public void ToString_NullInput_ReturnsNullLiteral()
+    {
+        const string expectedResult = "null";
+        int? testValue = null;
+        var actualResult = testValue.Ts();
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ToString_ValidInput_ReturnsInputAsString()
+    {
+        const string expectedResult = "32";
+        var testValue = 32;
+        var actualResult = testValue.Ts();
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
