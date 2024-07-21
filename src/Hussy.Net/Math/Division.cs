04cdd7f5c6aca07dca5ebee98d547156df90a35f
tacosontitan <contact@tacosontitan.com>
2024-07-20 20:55:27 -0500

    Moved division functions to the math folder.

--- src/Hussy.Net/Math/Division.cs
+++ src/Hussy.Net/Math/Division.cs
@@ -12,10 +12,11 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>2</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv2<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 2;
-    
+        where T : IDivisionOperators<T, T, T> =>
+        input / 2.To<T>();
+
     /// <summary>
     /// Divides the specified by input by <c>3</c>.
     /// </summary>
@@ -24,9 +25,10 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>3</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv3<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 3;
+        where T : IDivisionOperators<T, T, T> =>
+        input / 3.To<T>();
     
     /// <summary>
     /// Divides the specified by input by <c>4</c>.
@@ -36,9 +38,10 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>4</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv4<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 4;
+        where T : IDivisionOperators<T, T, T> =>
+        input / 4.To<T>();
     
     /// <summary>
     /// Divides the specified by input by <c>5</c>.
@@ -48,9 +51,10 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>2</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv5<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 5;
+        where T : IDivisionOperators<T, T, T> =>
+        input / 5.To<T>();
     
     /// <summary>
     /// Divides the specified by input by <c>6</c>.
@@ -60,9 +64,10 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>6</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv6<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 6;
+        where T : IDivisionOperators<T, T, T> =>
+        input / 6.To<T>();
     
     /// <summary>
     /// Divides the specified by input by <c>7</c>.
@@ -72,9 +77,10 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>7</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv7<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 7;
+        where T : IDivisionOperators<T, T, T> =>
+        input / 7.To<T>();
     
     /// <summary>
     /// Divides the specified by input by <c>8</c>.
@@ -84,9 +90,10 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>8</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv8<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 8;
+        where T : IDivisionOperators<T, T, T> =>
+        input / 8.To<T>();
     
     /// <summary>
     /// Divides the specified by input by <c>9</c>.
@@ -96,9 +103,10 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>9</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv9<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 9;
+        where T : IDivisionOperators<T, T, T> =>
+        input / 9.To<T>();
     
     /// <summary>
     /// Divides the specified by input by <c>10</c>.
@@ -108,7 +116,8 @@ public static partial class Hussy
     /// <returns>
     ///     The specified <paramref name="input"/> divided by <c>10</c>.
     /// </returns>
+    /// <remarks>This method is designed to shorten functions being passed as delegates.</remarks>
     public static T Dv10<T>(T input)
-        where T : IDivisionOperators<T, int, T> =>
-        input / 10;
+        where T : IDivisionOperators<T, T, T> =>
+        input / 10.To<T>();
 }
---
+++ test/Hussy.Net.Tests/Math/DivisionTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Hussy.Net.Tests.Math;
+
+public class DivisionTests
+{
+    private const double TestInput = 100;
+    
+    [Theory]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
+    [InlineData(10)]
+    public void Division_ProducesCorrectResult(double target)
+    {
+        var expectedResult = TestInput / target;
+        var testFunction = GetFunction(target);
+        var actualResult = testFunction(TestInput);
+        
+        Assert.Equal(expectedResult, actualResult);
+
+#pragma warning disable CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
+        
+        static Func<double, double> GetFunction(double target) => target switch
+        {
+            2 => Dv2,
+            3 => Dv3,
+            4 => Dv4,
+            5 => Dv5,
+            6 => Dv6,
+            7 => Dv7,
+            8 => Dv8,
+            9 => Dv9,
+            10 => Dv10
+        };
+        
+#pragma warning restore CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
+    }
+}
